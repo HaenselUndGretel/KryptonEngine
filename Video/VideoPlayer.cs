bea35bf95123fdac0d22131a4af86b5320523f3b
2Light <Kamui1337@googlemail.com>
2014-08-06 14:02:19 +0200

    Add VideoPlayer

---
+++ Video/VideoPlayer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace KryptonEngine
+{
+    static class CutScenePlayer
+    {
+        static Video Ga = null;
+        static Video Fables = null;
+        static Video Outro = null;
+
+        public static VideoPlayer player;
+
+        static public void Play(string scene)
+        {
+             Video tempVideo = null;
+             switch (scene)
+             {
+                 case "Ga":
+                     tempVideo = KryptonEngine.EngineSettings.Content.Load<Video>("video\\ga");
+                     break;
+                 case "Fables":
+                     tempVideo = KryptonEngine.EngineSettings.Content.Load<Video>("video\\fables");
+                     break;
+                 case "Outro":
+                     tempVideo = KryptonEngine.EngineSettings.Content.Load<Video>("video\\outro");
+                     break;
+             }
+
+            player.Play(tempVideo);
+        }
+
+        static void Draw(SpriteBatch pSpritBatch)
+        {
+            pSpritBatch.Draw(player.GetTexture(),Vector2.Zero,Color.White);
+        }
+        
+
+    }
+}
